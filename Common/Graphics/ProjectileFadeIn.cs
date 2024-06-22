b169dd15d00c612f7c996b5403154b54b1a12588
shnakamura <joaovtnakmura@gmail.com>
2024-06-22 02:09:37 -0300

    Defragments components into more primitive use cases

---
+++ Common/Graphics/ProjectileFadeIn.cs
@@ -0,0 +1,74 @@
+using AbyssalBlessings.Common.Projectiles.Components;
+using Terraria;
+using Terraria.DataStructures;
+
+namespace AbyssalBlessings.Common.Graphics;
+
+/// <summary>
+///     Handles the fading in of a <see cref="Projectile"/>.
+/// </summary>
+public sealed class ProjectileFadeIn : ProjectileComponent
+{
+    public struct FadeData
+    {
+        /// <summary>
+        ///     The initial opacity of the projectile.
+        /// </summary>
+        /// <remarks>
+        ///     Ranges from 0 (Visible) - 255 (Invisible).
+        /// </remarks>
+        public int Initial { get; set; } = 255;
+        
+        /// <summary>
+        ///     The step amount used to increase the opacity.
+        /// </summary>
+        public int Step { get; set; } = 5;
+        
+        /// <summary>
+        ///     Whether the projectile is fading in or not.
+        /// </summary>
+        public bool Fading { get; set; }
+
+        public FadeData() { }
+    }
+
+    public FadeData Data = new();
+
+    public override void SetDefaults(Projectile entity) {
+        if (!Enabled) {
+            return;
+        }
+
+        entity.alpha = Data.Initial;
+    }
+
+    public override void OnSpawn(Projectile projectile, IEntitySource source) {
+        if (!Enabled) {
+            return;
+        }
+
+        Data.Fading = true;
+    }
+
+    public override void OnKill(Projectile projectile, int timeLeft) {
+        if (!Enabled) {
+            return;
+        }
+        
+        Data.Fading = false;
+    }
+
+    public override void AI(Projectile projectile) {
+        if (!Enabled || !Data.Fading) {
+            return;
+        }
+
+        projectile.alpha -= Data.Step;
+
+        if (projectile.alpha > 0) {
+            return;
+        }
+
+        Data.Fading = false;
+    }
+}
